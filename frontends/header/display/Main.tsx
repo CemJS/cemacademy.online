534ab2d7c372ca7d06877e2668bb12973db2678f
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-10-23 17:31:46 +0300

    .

--- frontends/header/display/Main.tsx
+++ frontends/header/display/Main.tsx
@@ -1,6 +1,38 @@
 import { Cemjsx } from "cemjs-all"
 import logoWhite from '@svg/logoWhite.svg'
+import menu from 'json/menu.json'
 
+const RenderMenu = function ({ items }) {
+  return (
+    <ul class="header_navList">
+      {
+        items.map((item) => {
+          return (
+            <li>
+              <a href={item.link} class="header_navList__item">{item.name}</a>
+            </li>
+          )
+        })
+      }
+    </ul>
+  )
+}
+
+const RenderMobileMenu = function ({ items }) {
+  return (
+    <ul class="header_mobile-list">
+      {
+        items.map((item) => {
+          return (
+            <li>
+              <a href={item.link} class="header_mobile-list__item">{item.name}</a>
+            </li>
+          )
+        })
+      }
+    </ul>
+  )
+}
 
 export default function () {
   return (
@@ -9,28 +41,37 @@ export default function () {
         <img src={logoWhite} alt="Academy Crypto Emergency" />
       </a>
       <nav class="header_nav">
-        <ul class="header_navList">
-          <li>
-            <a class="header_navList__item" href="#">Программа</a>
-          </li>
-          <li>
-            <a class="header_navList__item" href="#">Эксперты</a>
-          </li>
-          <li>
-            <a class="header_navList__item" href="#">Тарифы</a>
-          </li>
-          <li>
-            <a class="header_navList__item" href="#">Результаты</a>
-          </li>
-        </ul>
+        <RenderMenu items={menu} />
       </nav>
-      <a
-        class="btn btn__green"
-        href="https://t.me/dmitriibelov"
-        target="_blank"
-      >
-        Записаться
-      </a>
+      <div class="header_btns">
+        <a
+          class="btn btn__green"
+          href="https://t.me/dmitriibelov"
+          target="_blank"
+        >
+          Записаться
+        </a>
+        <div
+          class="header_burger"
+          ref="burger"
+          onclick={() => {
+            this.Ref.burger.classList.toggle('header_burger__active')
+            this.Ref.mobileMenu.classList.toggle('header_mobile-menu__active')
+            this.init()
+          }}
+        >
+          <span class="header_burger__item"></span>
+          <span class="header_burger__item"></span>
+          <span class="header_burger__item"></span>
+        </div>
+      </div>
+
+      <div class="header_mobile-menu" ref="mobileMenu">
+        <RenderMobileMenu items={menu} />
+      </div>
+
+
+
     </div>
   )
 }
