6fe70811096d10fc3c256324300c32b942692394
Betarost <betarost@yandex.ru>
2023-11-07 15:42:08 +0300

    .

--- frontends/modalSale/display/Main.tsx
+++ frontends/modalSale/display/Main.tsx
@@ -4,16 +4,16 @@ import tap from '@svg/icons/tap.svg'
 export default function () {
     return (
         <div class="sale_content">
-            <p class="sale_text">Успей приобрести курс со скидкой <span class="sale_text__accent">15%</span></p>
+            <p class="sale_text">Приобрети курс со скидкой <span class="sale_text__accent">{this.Variable.bonusProc}%</span></p>
             <button
                 class="btn btn__white"
                 onclick={() => this.Fn.initOne({
                     name: "modalOrder",
-                    data: { title: "Записаться на курс" }
+                    data: { title: `Записаться на курс! Скидка ${this.Variable.bonusProc}%` }
                 })}
             >
 
-                Купить прямо сейчас
+                Купить сейчас
                 <img src={tap} alt="Успей приобрести курс от Academy Crypto Emergency!" />
             </button>
 
